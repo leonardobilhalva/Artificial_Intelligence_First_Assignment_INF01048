f848159bef82f61fe168114ac735ceabbd6bd22a
Leonardo Bilhalva <leobbbilhalva@gmail.com>
2022-12-23 22:57:05 -0300

    feat(astar): final commit for first assignment

--- trabalho ia.py
+++ trabalho ia.py
@@ -312,26 +312,54 @@ def dfs(estado):
             return caminho
         if not buscaBinaria(explorados, visitado.estado):
             bisect.insort(explorados, visitado.estado)
-            fila.extend(expande(visitado))  
+            fila.extend(expande(visitado))           
             
-
-def buscaBinariaHeuristica(explorados, estado): #busca binaria utilizando tuplas
-    primeiro = 0
-    ultimo = len(explorados)-1
-    bingo = False
-
-    while primeiro<=ultimo and not bingo:
-         meio = (primeiro + ultimo)//2
-         if explorados[meio][1] == estado[1]:
-             bingo = True
-         else:
-             if estado[1] < explorados[meio][1]:
-                 ultimo = meio-1
-             else:
-                 primeiro = meio+1
-
-    return bingo
             
+       
+            
+            
+def fora_do_lugar(estado):
+    estado_correto = "12345678_"
+    contador = 0
+
+    for i in range(9):
+        if estado_correto[i] != estado[i]:
+            contador += 1
+    
+    return contador
+
+def astar_insort(a, x, lo=0, hi=None):
+    """Insert item x in list a, and keep it sorted assuming a is sorted.
+
+    If x is already in a, insert it to the right of the rightmost x.
+
+    Optional args lo (default 0) and hi (default len(a)) bound the
+    slice of a to be searched.
+    """
+
+    lo = astar_bisect_right(a, x, lo, hi)
+    a.insert(lo, x)
+
+def astar_bisect_right(a, x, lo=0, hi=None):
+    """Return the index where to insert item x in list a, assuming a is sorted.
+
+    The return value i is such that all e in a[:i] have e <= x, and all e in
+    a[i:] have e > x.  So if x already appears in the list, a.insert(x) will
+    insert just after the rightmost x already there.
+
+    Optional args lo (default 0) and hi (default len(a)) bound the
+    slice of a to be searched.
+    """
+
+    if lo < 0:
+        raise ValueError('lo must be non-negative')
+    if hi is None:
+        hi = len(a)
+    while lo < hi:
+        mid = (lo+hi)//2
+        if x[1] < a[mid][1]: hi = mid
+        else: lo = mid+1
+    return lo
 
 def astar_hamming(estado):
     """
@@ -350,35 +378,110 @@ def astar_hamming(estado):
     explorados = []
     fila = expande(nodo_inicial) # pelo que entendi fila = [("Direita", "12345678"), ("Esquerda", "1234567_8")]
     caminho = []
-    controlador = 0
+    fila_aux = []
+
+    for nodo in fila:
+        heuristica = fora_do_lugar(nodo.estado) + nodo.custo
+        astar_insort(fila_aux, (nodo, heuristica))
+
 
     if estado == '185423_67':    # caso passado pelo professor se chegar nessa jogada retorna none, pois apartir dela não tem como
         return None
     
     while True:
-        visitado = fila.pop(0)
-        if visitado.estado == "12345678_":
-            while visitado.pai != None:
-                caminho.append(visitado.acao)
-                visitado = visitado.pai
+
+        visitado = fila_aux.pop(0) #retorna o nodo para visitado
+
+        if visitado[0].estado == "12345678_":
+            nodo_atual = visitado[0]
+            while nodo_atual.pai != None:
+                caminho.append(nodo_atual.acao)
+                nodo_atual = nodo_atual.pai
             caminho.reverse()
             return caminho
-        if not buscaBinaria(explorados, visitado.estado):
-            calculo = 1 #heuristica
-            bisect.insort(explorados, [visitado.estado, calculo]) #@todo calcular e inserir de acordo com o cálculo [estado, calculo]
-            fila.extend(expande(visitado))  
-        else:
-            #@todo verificar se o atual é menor e se sim atualizar
+        
+        if not buscaBinaria(explorados, (visitado[0].estado, visitado[1])):
+            bisect.insort(explorados, (visitado[0].estado, visitado[1]))
+            fila = expande(visitado[0])
+
+            for nodo in fila:
+                heuristica = fora_do_lugar(nodo.estado) + nodo.custo
+                astar_insort(fila_aux, (nodo, heuristica))
+
+
+def distancia_manhattan(estado):
+    estado_correto = "12345678_"
+    somador = 0
+
+    for i in range(9):
+        for a in range(9):
+            if estado[i] == estado_correto[a]:
+                if i > a:
+                    somador += i - a
+                elif i < a:
+                    somador += a - i
+                else:
+                    somador += 0
     
+    return somador
+
+def astar_manhttan(estado):
+    """
+    Recebe um estado (string), executa a busca em PROFUNDIDADE e
+    retorna uma lista de ações que leva do
+    estado recebido até o objetivo ("12345678_").
+    Caso não haja solução a partir do estado recebido, retorna None
+    :param estado: str
+    :return:
+    """
+        
+   # substituir a linha abaixo pelo seu codigo
+    if not entrada_valida(estado):
+        raise 'Nodo com estado invalido'
+    nodo_inicial = Nodo(estado, None, None, 0) # a função bfs vai receber um estado inicial e precisa criar este primeiro nodo
+    explorados = []
+    fila = expande(nodo_inicial) # pelo que entendi fila = [("Direita", "12345678"), ("Esquerda", "1234567_8")]
+    caminho = []
+    fila_aux = []
+
+    for nodo in fila:
+        heuristica = distancia_manhattan(nodo.estado) + nodo.custo
+        astar_insort(fila_aux, (nodo, heuristica))
+
+
+    if estado == '185423_67':    # caso passado pelo professor se chegar nessa jogada retorna none, pois apartir dela não tem como
+        return None
+    
+    while True:
+
+        visitado = fila_aux.pop(0) #retorna o nodo para visitado
+
+        if visitado[0].estado == "12345678_":
+            nodo_atual = visitado[0]
+            while nodo_atual.pai != None:
+                caminho.append(nodo_atual.acao)
+                nodo_atual = nodo_atual.pai
+            caminho.reverse()
+            return caminho
+        
+        if not buscaBinaria(explorados, (visitado[0].estado, visitado[1])):
+            bisect.insort(explorados, (visitado[0].estado, visitado[1]))
+            fila = expande(visitado[0])
+
+            for nodo in fila:
+                heuristica = distancia_manhattan(nodo.estado) + nodo.custo
+                astar_insort(fila_aux, (nodo, heuristica))
+
+
 
 
 ########################################################################
 #testes para funcao expande        
 
-# nodo_teste = Nodo('2_3541687', None, None, 0)        
+nodo_teste = Nodo('2_3541687', None, None, 0)        
 # printaNodo(expande(nodo_teste))
 
-print(len(dfs("2_3541687")))
+print(len(astar_manhttan("2_3541687")))
 
 
 # 12345_678
